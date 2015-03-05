b643d56806ae7c5c4c3a31767bc02404e9b5c66c
Florian Kuhn <kuhn@ids-mannheim.de>
2015-03-05 17:00:05 +0100

    removed old option handling

--- src/batchxslt/dgdstart.py
+++ src/batchxslt/dgdstart.py
@@ -2,37 +2,51 @@ __author__ = 'kuhn'
 __doc__ = "starts transformation with dgd configuration"
 
 import sys
-
 import processor
+import logging
+from optparse import OptionParser
 
 
 if __name__ == '__main__':
-
+    logging.basicConfig(level="info")
     # read the configuration file from command line
-    configuration = processor.ConfigParser(sys.argv[2])
-    # read option string (1st argument)
-    option = sys.argv[1].strip().lower()
 
-    # set the xslt processor
-    processor = processor.XSLBatchProcessor(
-        configuration.globals.get("processor"))
+    # define options
+    optionparse = OptionParser()
 
-    # start the processor for corpus, event and speaker
-    print "starting corpus transformations"
-    processor.start(configuration.corpus.get("stylesheet"),
-                     configuration.corpus.get("indirectory"),
-                     configuration.corpus.get("prefix"),
-                     configuration.corpus.get("outdirectory"), option)
-    print "starting event transformations"
-    processor.start(configuration.event.get("stylesheet"),
-                     configuration.event.get("indirectory"),
-                     configuration.event.get("prefix"),
-                     configuration.event.get("outdirectory"), option)
-    print "starting speaker transformation"
-    processor.start(configuration.speaker.get("stylesheet"),
-                    configuration.speaker.get("indirectory"),
-                    configuration.speaker.get("prefix"),
-                    configuration.speaker.get("outdirectory"), option)
+try:
+    configuration = processor.ConfigParser(sys.argv[1])
+except IndexError:
+    logging.error("No configuration file given. Aborting")
+    sys.exit()
+except IOError:
+    logging.error("configuration File" + sys.argv[1] + " not found.")
+    sys.exit()
 
-    print "finished tranformations"
+# TODO: use a better option handling.
+
+# set the xslt processor
+proc = processor.XSLBatchProcessor(
+    configuration.globals.get("processor"))
+
+# start the processor for corpus, event and speaker
+print "starting corpus transformations"
+proc.start(configuration.corpus.get("stylesheet"),
+           configuration.corpus.get("indirectory"),
+           configuration.corpus.get("prefix"),
+           configuration.corpus.get("outdirectory"))
+print "starting event transformations"
+proc.start(configuration.event.get("stylesheet"),
+           configuration.event.get("indirectory"),
+           configuration.event.get("prefix"),
+           configuration.event.get("outdirectory"))
+print "starting speaker transformation"
+proc.start(configuration.speaker.get("stylesheet"),
+           configuration.speaker.get("indirectory"),
+           configuration.speaker.get("prefix"),
+           configuration.speaker.get("outdirectory"))
+
+print "finished tranformations"
+
+
 
