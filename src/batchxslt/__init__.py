e5685427905e47178bd9319f6d420787ff67ad5c
Florian Kuhn <kuhn@ids-mannheim.de>
2015-03-05 23:53:51 +0100

    prepared main() for further additions

--- src/batchxslt/__init__.py
+++ src/batchxslt/__init__.py
@@ -2,9 +2,21 @@
 __author__ = "fkuhn"
 __doc__ = "batchxslt 0.1 Refer to /doc for a tutorial."
 
+import processor
+import logging
+import sys
 
 def main():
     """
     console entry point
     """
-    print "batchxslt 0.1"
+    print "batchxslt 0.2"
+
+    try:
+        configuration = processor.ConfigParser(sys.argv[1])
+    except IndexError:
+        logging.error("No configuration file given. Aborting")
+        sys.exit()
+    except IOError:
+        logging.error("configuration File" + sys.argv[1] + " not found.")
+        sys.exit()
