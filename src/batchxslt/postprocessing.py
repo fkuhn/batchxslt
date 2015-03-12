c07237f6732d58d2128c7d5a8b9b5f2bcb45ce95
Florian Kuhn <kuhn@ids-mannheim.de>
2015-03-12 19:06:50 +0100

    torplace-data is now stored in a dict

--- src/batchxslt/postprocessing.py
+++ src/batchxslt/postprocessing.py
@@ -17,8 +17,12 @@ class PostProcessor(object):
         :return:
         """
         # non value processing: define mappings
-        self.__notfiled = {'nicht dokumentiert': 'not filed'}
-        self.__nonvalue = {'nicht vorhanden': 'none'}
+        self.__notfiled = {'nicht dokumentiert': 'not filed',
+                           'Nicht dokumentiert': 'not filed',
+                           'Nicht relevant': 'not filed'}
+        self.__nonvalue = {'nicht vorhanden': 'none',
+                           'Nicht vorhanden': 'none'}
+        self.__outputpath = '/home/kuhn/Data/IDS/cmditest'
 
     def replace_nonvalues(self, filename):
         """
@@ -31,27 +35,32 @@ class PostProcessor(object):
         """
 
         if os.path.isfile(filename):
-            self.__rplace_nv(etree.parse(filename), filename)
+            fobj = codecs.open(filename)
+            self.__rplace_nv(etree.parse(fobj), filename)
         else:
             flist = os.listdir(filename)
-            for fn in flist:
-                self.__rplace_nv(etree.parse(fn), fn)
+            for fname in flist:
+                fobj = codecs.open(filename + '/' + fname)
+                self.__rplace_nv(etree.parse(fobj), fname)
 
-    def __rplace_nv(self, etreeobj, filename):
+    def __rplace_nv(self, etreeobj, fname):
         """
 
-        :param file:
+        :param etreeobj, fname:
         :return:
         """
 
         for element in etreeobj.getroot().iter():
-            if element.text.lower() in self.__nonvalue:
-                element.text = self.__nonvalue.get(element.text.lower())
-            if element.text.lower() in self.__notfiled:
-                element.text = self.__notfiled.get(element.text.lower())
+            if element.text is not None:
+                if element.text.lower() in self.__nonvalue:
+                    element.text = self.__nonvalue.get(element.text.lower())
+                if element.text.lower() in self.__notfiled:
+                    element.text = self.__notfiled.get(element.text.lower())
 
-        fobject = codecs.open(filename, mode='w', encoding='utf-8', )
-        etreeobj.write(fobject, method='XML', pretty_print=True,
-                       encoding='utf-8')
-
+        fobject = codecs.open(self.__outputpath + '/' + fname,
+                              mode='w+')
+        fstring = etree.tostring(etreeobj, method='XML', pretty_print=True,
+                                 encoding='utf-8')
+        fobject.write(fstring)
         fobject.close()
+
