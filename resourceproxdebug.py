7cb4da27549aff4381369ac89e6b703aee324ba9
kuhn <kuhn@ids-mannheim.de>
2015-04-30 17:40:26 +0200

    env for writing a test file

--- resourceproxdebug.py
+++ resourceproxdebug.py
@@ -7,11 +7,13 @@ from batchxslt import cmdiresource
 corpus = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/corpus/"
 event = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/event/"
 speakers = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/speakers/"
+transcripts = "/home/kuhn/Data/IDS/svn/dgd2_data/transcripts/"
 
-resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers)
+resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers, transcripts)
 
 resourcetree.build_resourceproxy()
 
+resourcetree.write_xml('FOLK_E_00020', '/tmp/folktest.xml')
 
 
 
