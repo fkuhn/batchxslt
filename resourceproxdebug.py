3347f622a7df58eeea0148fcd15e4eefbaa7d64a
Florian Kuhn <kuhn@ids-mannheim.de>
2015-05-02 12:17:39 +0200

    added dgd resources

--- resourceproxdebug.py
+++ resourceproxdebug.py
@@ -4,12 +4,19 @@ __doc__ = "test env for debugging of resource proxy generation"
 from batchxslt import cmdiresource
 
 # set_trace()
-corpus = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/corpus/"
-event = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/event/"
-speakers = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdiOutBeta2/speakers/"
-transcripts = "/home/kuhn/Data/IDS/svn/dgd2_data/transcripts/"
 
-resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers, transcripts)
+
+dgd_corpus = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/corpora/"
+dgd_event = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/events/"
+dgd_speakers = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/speakers/"
+
+cmdi_corpus = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/dgd2cmdi/cmdiOutput/corpus/"
+cmdi_event = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/dgd2cmdi/cmdiOutput/event/"
+cmdi_speakers = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/dgd2cmdi/cmdiOutput/speakers/"
+
+transcripts = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/transcripts/"
+
+resourcetree = cmdiresource.ResourceTreeCollection(cmdi_corpus, cmdi_event, cmdi_speakers, transcripts)
 
 resourcetree.build_resourceproxy()
 
