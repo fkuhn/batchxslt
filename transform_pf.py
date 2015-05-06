1fdfb4ccaf76e6a8007e135516f4a29754536eba
kuhn <kuhn@ids-mannheim.de>
2015-05-06 11:22:22 +0200

    changed types attribute for nodes to access metadata types more efficiently

--- transform_pf.py
+++ transform_pf.py
@@ -9,14 +9,30 @@ dgd_corpus = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/corpora/extern"
 dgd_events = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/events/extern"
 dgd_speakers = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/speakers/extern"
 
-xsl_corpus = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdCorpus2cmdi.xsl"
-xsl_events = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdEvent2cmdi.xsl"
-xsl_speakers = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdSpeaker2cmdi.xsl"
+corpus_xsl = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdCorpus2cmdi.xsl"
+event_xsl = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdEvent2cmdi.xsl"
+speaker_xsl = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/xslt/dgdSpeaker2cmdi.xsl"
 
+saxon_jar = "/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/batchxslt/saxon/saxon9he.jar"
 
 pf_corpus = os.path.join(dgd_corpus, 'PF--_extern.xml')
 pf_events = os.path.join(dgd_events, 'PF')
 pf_speakers = os.path.join(dgd_speakers, 'PF')
 
+xsl_processor = processor.XSLBatchProcessor(saxon_jar)
 
+xsl_processor.transform(corpus_xsl, pf_corpus, "cmdi_", '/tmp/cmdi/PF/corpus/')
 
+xsl_processor.transform(event_xsl, pf_events, "cmdi_", '/tmp/cmdi/PF/events/')
+
+xsl_processor.transform(speaker_xsl, pf_speakers, "cmdi_", '/tmp/cmdi/PF/speakers/')
+
+from batchxslt import cmdiresource
+
+corpus = "/tmp/cmdi/PF/corpus/"
+event = "/tmp/cmdi/PF/events/"
+speakers = "//tmp/cmdi/PF/speakers/"
+transcripts = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/transcripts/"
+
+resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers, transcripts)
+resourcetree.build_resourceproxy()
