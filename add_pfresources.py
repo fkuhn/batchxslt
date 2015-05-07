e5a10b96fa67b97725174984ef3c3f1455769757
kuhn <kuhn@ids-mannheim.de>
2015-05-07 18:22:19 +0200

    added media file extraction and reference

--- add_pfresources.py
+++ add_pfresources.py
@@ -5,9 +5,9 @@ import os
 
 from batchxslt import cmdiresource
 
-corpus = "/tmp/cmdi/PF/corpus/"
-event = "/tmp/cmdi/PF/events/"
-speakers = "//tmp/cmdi/PF/speakers/"
+corpus = "/tmp/cmdi/corpus/"
+event = "/tmp/cmdi/events/"
+speakers = "//tmp/cmdi/speakers/"
 transcripts = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/transcripts/"
 
 cmdi_final = '/tmp/cmdi/cmdiPF/'
@@ -22,12 +22,15 @@ for nodename in resourcetree.nodes_iter():
     elif resourcetree.node.get(nodename).get('type') == 'corpus':
         resourcetree.define_parts(nodename)
 
+# for nodename in resourcetree.nodes_iter():
+#     if resourcetree.node.get(nodename).get('type') == 'event':
+#         resourcetree.event2speaker(nodename)
+
 for nodename in resourcetree.nodes_iter():
-    if resourcetree.node.get(nodename).get('type') == 'event':
+    if resourcetree.node.get(nodename).get('type') == 'speaker':
         resourcetree.speaker2event(nodename)
 
 # FIXME: build_parts is not working
-
 
 for nodename in resourcetree.nodes_iter():
 
