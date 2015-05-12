5ec448f98c9a131b925e291f19422848d8eed885
kuhn <kuhn@ids-mannheim.de>
2015-05-12 11:59:46 +0200

    added a fixed relative path

--- src/batchxslt/cmdiheader.py
+++ src/batchxslt/cmdiheader.py
@@ -1,30 +1,37 @@
-
 __author__ = 'kuhn'
+__doc__ = """<Header>
+      <MdCreator>DGD2CMDI</MdCreator>
+      <MdCreationDate>2015-05-07+02:00</MdCreationDate>
+      <MdSelfLink>file:/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/metadata/events/extern/PF/PF--_E_00127_extern.xml</MdSelfLink>
+      <MdProfile>clarin.eu:cr1:p_1430905751592</MdProfile>
+      <MdCollectionDisplayName>AGD</MdCollectionDisplayName>
+        </Header>"""
 
 import logging
 import os
 from lxml import etree
 import mimetypes
 
+MDCREATOR = 'batchxslt.py'
+VALIDIDS = {'event': 'clarin.eu:cr1:p_1430905751604', 'corpus': 'clarin.eu:cr1:p_1430905751603'}
+SVNROOT = 'dgd2_data/dgd2cmdi/cmdi/PF/'
 
 
-class cmdiHeader(object):
+def define_header(cmdinode, resourcetree):
+    """
+    sets MdSelfLink and tests if MdProfile ID is valid
+    :param cmdinode:
+    :return:
+    """
+    cmdifilename = resourcetree.node.get(cmdinode).get('filename')
+    cmdiheader = resourcetree.node.get(cmdinode).get('etreeobject').getroot().find('Header')
 
-    def __init__(self):
+    cmdimdselflink = cmdiheader.find('MdSelfLink')
+    cmdiprofile = cmdiheader.find('MdProfile')
 
+    if cmdiprofile.text not in VALIDIDS.itervalues():
+        logging.error('The profile id of MdProfile is not valid: ' + cmdifilename.split('/')[-1])
 
-    def set_header(self, cmdifile):
-        """
-        set values for all elements in the header of a cmdi file
-            <Header>
-            <MdCreator>gewiss-coma2cmdi.xsl (Daniel Jettka, CLARIN-GeWiss-KP)</MdCreator>
-            <MdCreationDate>2014-02-13</MdCreationDate>
-            <MdSelfLink>http://hdl.handle.net/10932/00-01FB-53D5-A740-D401-9</MdSelfLink>
-            <MdProfile>clarin.eu:cr1:p_1361876010680</MdProfile>
-            <MdCollectionDisplayName>Institut fuer Deutsche Sprache,
-            CLARIN-D Zentrum, Mannheim</MdCollectionDisplayName>
-        </Header>
-        :param cmdifile:
-        :return:
-        """
-        cmdi_etree = etree.parse(cmdifile)
+    # get the path
+
+    cmdimdselflink.text = SVNROOT + cmdifilename
