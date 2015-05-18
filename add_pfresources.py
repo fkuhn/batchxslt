fa0ba802cdadd2c2ca103496dbd7124e111d35e4
Florian Kuhn <fkuhn@posteo.de>
2015-05-18 20:14:07 +0200

    changed id assignment for proxies

--- add_pfresources.py
+++ add_pfresources.py
@@ -15,7 +15,15 @@ cmdi_final = '/tmp/cmdi/cmdiPF/'
 
 resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers, transcripts)
 
+# give ids to nodes
+counter = 0
+for node in resourcetree.nodes_iter():
+    corpuslabel = node.split('_')[0].rstrip('-')
+    resourcetree.node.get(node).update({'id': corpuslabel + '_' + str(counter)})
+    counter += 1
 resourcetree.build_resourceproxy()
+
+
 
 for nodename in resourcetree.nodes_iter():
     if resourcetree.node.get(nodename).get('type') == 'event':
@@ -28,15 +36,9 @@ for nodename in resourcetree.nodes_iter():
         resourcetree.speaker2event(nodename)
 
 for nodename in resourcetree.nodes_iter():
-
-
     if resourcetree.node.get(nodename).get('type') == 'event':
-
         cmdiheader.define_header(nodename, resourcetree)
-
         resourcetree.write_cmdi(nodename, os.path.join(cmdi_final, nodename + '.cmdi'))
     elif resourcetree.node.get(nodename).get('type') == 'corpus':
-
         cmdiheader.define_header(nodename, resourcetree)
-
         resourcetree.write_cmdi(nodename, os.path.join(cmdi_final, nodename + '.cmdi'))
