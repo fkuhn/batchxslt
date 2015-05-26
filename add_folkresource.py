9f9bd5f1950b1965d4a507a78e321a37f7a8bbba
kuhn <kuhn@ids-mannheim.de>
2015-05-26 18:41:42 +0200

    fixme: paths not working

--- add_folkresource.py
+++ add_folkresource.py
@@ -1,4 +1,5 @@
 __author__ = 'kuhn'
+
 import os
 
 
@@ -6,14 +7,20 @@ from batchxslt import cmdiresource
 from batchxslt import cmdiheader
 
 corpus = "/tmp/cmdi/corpus/FOLK/"
-event = "/tmp/cmdi/events/"
-speakers = "//tmp/cmdi/speakers/"
+event = "/tmp/cmdi/events/FOLK"
+speakers = "//tmp/cmdi/speakers/FOLK"
 transcripts = "/home/kuhn/Data/IDS/svn_rev1233/dgd2_data/transcripts/"
 
-cmdi_final = '/home/kuhn/Data/IDS/svn/dgd2_data/dgd2cmdi/cmdi/FOLK/'
+cmdi_final = '/tmp/cmdi/cmdiFOLK/'
 
 resourcetree = cmdiresource.ResourceTreeCollection(corpus, event, speakers, transcripts)
 
+# give ids to nodes
+counter = 0
+for node in resourcetree.nodes_iter():
+    corpuslabel = node.split('_')[0].rstrip('-')
+    resourcetree.node.get(node).update({'id': corpuslabel + '_' + str(counter)})
+    counter += 1
 resourcetree.build_resourceproxy()
 
 for nodename in resourcetree.nodes_iter():
@@ -22,20 +29,14 @@ for nodename in resourcetree.nodes_iter():
     elif resourcetree.node.get(nodename).get('type') == 'corpus':
         resourcetree.define_parts(nodename)
 
-for nodename in resourcetree.nodes_iter():
-    if resourcetree.node.get(nodename).get('type') == 'speaker':
+for nodename, ndata in resourcetree.nodes_iter(data=True):
+    if ndata.get('type') == 'speaker':
         resourcetree.speaker2event(nodename)
 
-for nodename in resourcetree.nodes_iter():
-
-
-    if resourcetree.node.get(nodename).get('type') == 'event':
-
+for nodename, ndata in resourcetree.nodes_iter(data=True):
+    if ndata.get('type') == 'event':
         cmdiheader.define_header(nodename, resourcetree)
-
         resourcetree.write_cmdi(nodename, os.path.join(cmdi_final, nodename + '.cmdi'))
-    elif resourcetree.node.get(nodename).get('type') == 'corpus':
-
+    elif ndata.get('type') == 'corpus':
         cmdiheader.define_header(nodename, resourcetree)
-
         resourcetree.write_cmdi(nodename, os.path.join(cmdi_final, nodename + '.cmdi'))
