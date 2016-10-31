a0607956b07a322c3c56b9ab82ce7b8b3265ffd1
kuhn <kuhn@ids-mannheim.de>
2016-10-31 14:17:07 +0100

    cli script updated

--- src/dgd2cmdi/dgd2cmdi.py
+++ src/dgd2cmdi/dgd2cmdi.py
@@ -1,41 +1,28 @@
-from __future__ import unicode_literals
-
-import argparse
+#!/usr/bin/python
 import os
-import sys
-import config
+import codecs
+import argparse
+import yaml
 
-from prompt_toolkit import prompt
-from pygments.token import Token
-from prompt_toolkit.application import Application
-from prompt_toolkit.interface import CommandLineInterface
-
-
-#default config path
-CONF = os.path.abspath('/data/config/')+'config.yml'
-
-
-#defapplication = Application(
-#    layout=layout,
-#    key_bindings_registry=registry,
-
-    # Let's add mouse support as well.
- #   mouse_support=True,
-
-    # For fullscreen:
- #   use_alternate_screen=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('resources', help='a file containing paths of a resource (corpus, event, speaker) to transform')
 
 
 def main():
     """
-    This is the main function used for cli functionability.
+    Main Method for dgd to cmdi transformation
+    :return:
     """
+    args = parser.parse_args()
+    with codecs.open(args.resources, mode='r', encoding='utf-8') as resfile:
+        resources = yaml.safe_load(resfile)
+    xslt = resources.get('xslt')
+    print "Stylesheet paths:\n"
+    print "Corpus: {}".format(xslt['corpus'])
+    print "Event: {}".format(xslt['event'])
+    print "Speaker: {}".format(xslt['speaker'])
 
-    # configuration = config.load_configuration()
 
-    parser = argparse.ArgumentParser()
+if __name__ == '__main__':
+    main()
 
-    # transform()
-
-    # b) define a config file based transformation
-    # default case w/o parameters
