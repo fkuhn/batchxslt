d6addc3f2f1f723f86c97b9784ab5c1f8b83d731
Florian Kuhn <kuhn@ids-mannheim.de>
2016-11-17 08:43:47 +0100

    added call_processor, changed res path labeling.

--- src/dgd2cmdi/dgd2cmdi.py
+++ src/dgd2cmdi/dgd2cmdi.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
-import os
-import codecs
+"""=
+This module provides methods to read the configuration file,
+process the referenced resources and
+"""
 import argparse
+import codecs
+import os
+
 import yaml
 
 parser = argparse.ArgumentParser()
-parser.add_argument('resources', help='a file containing paths of a resource (corpus, event, speaker) to transform')
+parser.add_argument(
+    'resources', help='a file containing paths of a resource (corpus, event, speaker) to transform')
 
 
 def main():
@@ -16,26 +22,120 @@ def main():
     args = parser.parse_args()
     with codecs.open(args.resources, mode='r', encoding='utf-8') as resfile:
         resources = yaml.safe_load(resfile)
-    processor = resources['processor']
-    xslt = resources.get('stylesheets')
-    print "Processorpath: {}".format(processor)
-    print "Stylesheet paths:\n"
-    print "Corpus: {}".format(xslt['corpus'])
-    print "Event: {}".format(xslt['event'])
-    print "Speaker: {}".format(xslt['speaker'])
-    print resources.get('collection')
-    resdic = resources.get('collection')
-    for coll in resdic:
-        print resdic.get(coll).get('speaker')
-        
+    # processor = resources['processor']
+    # xslt = resources.get('stylesheets')
+    # print "Processorpath: {}".format(processor)
+    # print "Stylesheet paths:\n"
+    # print "Corpus: {}".format(xslt['corpus'])
+    # print "Event: {}".format(xslt['event'])
+    # print "Speaker: {}".format(xslt['speaker'])
+    # print resources.get('collection')
+    # resdic = resources.get('collection')
+    # for coll in resdic:
+    #    print resdic.get(coll).get('speaker')
+
         # Iterate over the resource and call the processor
+    transform(resources)
 
 
+def transform(resources):
+    """
+    calls the processor and refers to all resources
+    given in the configuration file.
+    """
+    # define vars from config file
+    processor = resources['processor']
+    stylesheets = resources['stylesheets']
+    collection = resources['collection']
+    outputinter = resources['output-inter']
+    outputfinal = resources['output-final']
 
-# if __name__ == '__main__':
-#    main()
+    for resource in collection:
 
-# TODO: configuration file processing
-# TODO: subcommand for each conversion step
-# TODO: 'transform' and 'add_dep'
+        corpus = collection.get(resource).get('corpus')
+        events = collection.get(resource).get('event')
+        speakers = collection.get(resource).get('speaker')
 
+        outputfolder = prepare_cpath(outputinter, resource)
+
+
+def call_processor(metafilepath, resourcetype, stylesheetdic, saxonpath, outputpath):
+    """
+    calls the xslt processor
+    """
+    metafilepath = os.path.abspath(metafilepath)
+    saxonpath = os.path.abspath(saxonpath)
+
+    if resourcetype == 'corpus':
+        stylesheetpath = os.path.abspath(stylesheetdic.get('C_XSL'))
+        outputpath = os.path.abspath(OUTPATHDIC.get('C'))
+        os.system("java -jar {} -s:{} -xsl:{} -o:{}".format(
+            saxonpath, metafilepath,
+            stylesheetpath, os.path.join(outputpath,
+                                         os.path.basename(metafilepath).split('.')[0] + '.cmdi')))
+    elif resourcetype == 'event':
+
+        stylesheetpath = os.path.abspath(stylesheetdic.get('E_XSL'))
+        outputpath = os.path.abspath(os.path.join(
+            OUTPATHDIC.get('E'), os.path.basename(metafilepath)))
+        for resource in os.listdir(metafilepath):
+            os.system("java -jar {} -s:{} -xsl:{} -o:{}".format(
+                saxonpath, os.path.join(metafilepath, resource),
+                stylesheetpath, os.path.join(outputpath,
+                                             '.'.join([resource.split('.')[0], 'cmdi']))))
+
+    elif resourcetype == 'speaker':
+
+        stylesheetpath = os.path.abspath(stylesheetdic.get('S_XSL'))
+        outputpath = os.path.abspath(os.path.join(
+            OUTPATHDIC.get('S'), os.path.basename(metafilepath)))
+        for resource in os.listdir(metafilepath):
+            os.system("java -jar {} -s:{} -xsl:{} -o:{}".format(
+                saxonpath, os.path.join(metafilepath, resource),
+                stylesheetpath, os.path.join(outputpath,
+                                             '.'.join([resource.split('.')[0], 'cmdi']))))
+    else:
+        raise ValueError()
+
+
+def finalize_resources():
+    """
+    The final step adding resource proxies, cmdi headers and speaker
+    informations in event metafiles.
+    """
+    pass
+
+# -------------------------------
+# Some helper methods and classes
+# -------------------------------
+
+
+def prepare_cpath(outfolder, cname):
+    """
+    creates a valid path to a resource collection
+    named after the corpus label.
+    """
+    if not os.path.isdir(os.path.join(outfolder, cname)):
+        os.mkdir(os.path.join(outfolder, cname))
+
+    return os.path.join(outfolder, cname)
+
+
+class CorpusIterator(object):
+    """
+    Iterator is initialized with a corpus path.
+    either returns files or, in case of event and speaker,
+    the corpus label directories
+    """
+
+    def __init__(self, resourcepath, resourcetype):
+        self.resourcepath = os.path.abspath(resourcepath)
+        self.resourcetype = resourcetype
+        self._files_iter = iter(os.listdir(self.resourcepath))
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        file_name = self._files_iter.next()
+        return os.path.join(self.resourcepath, file_name)
